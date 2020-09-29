10a1ece04f637e36f3182b7f51a186ea91f5985d
Norman <norman@yazio.com>
2020-09-30 00:58:17 +0100

    Add button to start / stop a running timer

--- client/pages/timers.tsx
+++ client/pages/timers.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../components/layout';
 import useSWR from "swr";
 import {differenceInDays, differenceInSeconds, format} from 'date-fns';
 import isToday from 'date-fns/isToday';
 import cn from 'classnames';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
-import {log} from "util";
+import { Pagination } from '../components/pagination';
 
 export const Timers = () => {
+    const forceUpdate = useForceUpdate();
+
     const router = useRouter();
-    console.log(router.query);
-    const url = 'https://localhost:8443/users/1/timers?order[dateStart]&page='+router.query.page;
+    const currentPage = Number(typeof router.query.page !== 'undefined' ? router.query.page : 1);
+    const url = 'https://localhost:8443/users/1/timers?order[dateStart]&page='+currentPage;
     const fetcher = (...args) => fetch(...args).then(res => res.json());
 
     const { data, error } = useSWR(url, fetcher)
@@ -27,6 +28,7 @@ export const Timers = () => {
 
     let timersNormalized = {};
     let totalSecondsPerDay = {};
+    let runningTimer = false;
     console.log(data);
     return (
         <Layout>
@@ -35,36 +37,14 @@ export const Timers = () => {
             typeof data['hydra:member'] === 'undefined' || data['hydra:member'].length === 0 ? <div>no data yet...</div> :
             <div className="mt-6">
                 <div>
-                    {data['hydra:member'].map((value, key) => {
-                        typeof value.date_end !== 'undefined' ? value.isRunning = false : value.isRunning = true;
-                        const dateStartStr = format(new Date(value.date_start), 'u-MM-dd');
-
-                        let diffInSeconds = 0;
-                        if (!value.isRunning) {
-                            diffInSeconds = differenceInSeconds(new Date(value.date_end), new Date(value.date_start));
-                        } else {
-                            diffInSeconds = differenceInSeconds(new Date(), new Date(value.date_start));
-                        }
-                        value.diffInSeconds = diffInSeconds;
-
-                        if (totalSecondsPerDay.hasOwnProperty(dateStartStr)) {
-                            totalSecondsPerDay[dateStartStr] += diffInSeconds
-                        } else {
-                            totalSecondsPerDay[dateStartStr] = diffInSeconds;
-                        }
-
-                        if (typeof timersNormalized[dateStartStr] === 'undefined') {
-                            timersNormalized[dateStartStr] = [];
-                        }
-                        timersNormalized[dateStartStr].push(value);
-                    })}
+                    {prepareTimerData(data, totalSecondsPerDay, timersNormalized)}
 
                     {Object.entries(timersNormalized).map(([dateStartString, timers]) => {
                         const dateStart = new Date(dateStartString);
 
                         const formattedDiffInMinPerDay = toHHMMSS(totalSecondsPerDay[dateStartString]);
                         return (
-                            <div key={dateStartString} className="bg-white p-2 border-gray-300 border-t">
+                            <div key={dateStartString} className="bg-white p-2 mb-1 border-gray-300 border-l-4 hover:border-teal-600">
                                 <>
                                     <div className="text-xs text-gray-500">{format(dateStart, 'dd LLLL uuuu' )}</div>
                                     <div className="flex flex-row items-center">
@@ -75,6 +55,7 @@ export const Timers = () => {
                                 </>
                                 {timers.map((timer, index) => {
                                     const formattedDiffInMinPerDay = toHHMMSS(timer.diffInSeconds);
+                                    timer.isRunning ? runningTimer = timer : null;
                                     return generateSubTimerHtml(timer, formattedDiffInMinPerDay);
                                 })}
                             </div>
@@ -82,53 +63,25 @@ export const Timers = () => {
                     })}
                 </div>
                 <div>
-                    <div className="flex flex-col items-center my-12">
-                        <div className="flex text-gray-700">
-                            {Number(router.query.page) > 1 ?
-                                <Link href={`http://localhost:3000/timers?page=${Number(router.query.page)-1}`}>
-                                    <a className="h-12 w-12 mr-1 flex justify-center items-center rounded-full cursor-pointer">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none"
-                                             viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                             stroke-linejoin="round" className="feather feather-chevron-left w-6 h-6">
-                                            <polyline points="15 18 9 12 15 6"></polyline>
-                                        </svg>
-                                    </a>
-                                </Link>
-                                :
-                                <div className="h-12 w-12 mr-1 flex justify-center items-center rounded-full cursor-pointer opacity-25">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none"
-                                         viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                         stroke-linejoin="round" className="feather feather-chevron-left w-6 h-6">
-                                        <polyline points="15 18 9 12 15 6"></polyline>
-                                    </svg>
-                                </div>
-                            }
-                            <div className="flex h-12 font-medium rounded-full bg-gray-200">
-                                {generatePagination(data, router.query.page)}
-                                <div className="w-12 h-12 md:hidden flex justify-center items-center cursor-pointer leading-5 transition duration-150 ease-in rounded-full bg-teal-600 text-white">{router.query.page}</div>
-                            </div>
-                            {Number(router.query.page) <= Math.ceil(data['hydra:totalItems'] / 30) - 1 ?
-                                <Link href={`http://localhost:3000/timers?page=${Number(router.query.page)+1}`}>
-                                    <a className="h-12 w-12 mr-1 flex justify-center items-center rounded-full cursor-pointer">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none"
-                                             viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                             stroke-linejoin="round" className="feather feather-chevron-right w-6 h-6">
-                                            <polyline points="9 18 15 12 9 6"></polyline>
-                                        </svg>
-                                    </a>
-                                </Link>
-                                :
-                                <div className="h-12 w-12 mr-1 flex justify-center items-center rounded-full cursor-pointer opacity-25">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none"
-                                         viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                         stroke-linejoin="round" className="feather feather-chevron-right w-6 h-6">
-                                        <polyline points="9 18 15 12 9 6"></polyline>
-                                    </svg>
-                                </div>
-                            }
-                        </div>
-                    </div>
+                    <Pagination
+                        currentPage={currentPage}
+                        totalPages={Math.ceil(data['hydra:totalItems'] / 30)}
+                    />
                 </div>
+                {/*<Notification/>*/}
+                {runningTimer ?
+                    <div
+                        className="fixed bottom-0 notification bottom bg-red-500 rounded-full p-4 border-teal-400 border-2 shadow-md cursor-pointer"
+                        onClick={() => stopTimer(forceUpdate, runningTimer)}>
+                        <img src="../images/icons/icons8-stop-48.png" width="30" height="30" alt="Stop Timer"/>
+                    </div>
+                    :
+                    <div
+                        className="fixed bottom-0 notification bottom bg-teal-500 rounded-full p-4 border-teal-400 border-2 shadow-md cursor-pointer"
+                        onClick={() => startTimer(forceUpdate)}>
+                        <img src="../images/icons/icons8-play-100.png" width="30" height="30" alt="Start Timer"/>
+                    </div>
+                }
             </div>
             }
         </Layout>
@@ -136,13 +89,12 @@ export const Timers = () => {
 }
 
 //TO-DO
-//running timer icon
-//running timer - show update minutes and seconds
-//bottom right play icon to start work timer. Show stop icon if timer is running on all pages
 /**
  * only allow user to see its own entries!
- * menu - highlight current selected menu element
- * cleanup paging
+ * play button to start timer.
+ * running timer - update minutes and seconds
+ * jump to first page when adding timer
+ * add timer and refresh directly to show new timer
  */
 
 //DONE
@@ -152,7 +104,64 @@ export const Timers = () => {
  * show seconds
  * add timer fixtures
  * paging of timers
+ *
+ * cleanup paging
+ * add hover effect on timer rows
+ * menu - highlight current selected menu element
+ * format start and endtime on timer nicely
+ * bottom right play icon to start work timer. Show stop icon if timer is running on all pages
+ * stop timer on icon click and refresh view
  */
+
+const stopTimer = async (forceUpdate, timer) => {
+    timer.date_end = new Date();
+    const res = await fetch('https://localhost:8443/timers/'+timer.id, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/merge-patch+json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(timer),
+    })
+
+    const json = await res.json()
+    if (json.errors) {
+        console.error(json.errors)
+        throw new Error('Failed to fetch API')
+    }
+    forceUpdate();
+}
+
+const startTimer = async (forceUpdate) => {
+    const timer = {
+        user: '/users/1',
+        date_start: new Date(),
+        date_end: null,
+        timerType: 'punch'
+    }
+    const res = await fetch('https://localhost:8443/timers', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/ld+json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(timer),
+    })
+
+    const json = await res.json()
+    if (json.errors) {
+        console.error(json.errors)
+        throw new Error('Failed to fetch API')
+    }
+    console.log(json);
+    forceUpdate();
+}
+
+function useForceUpdate(){
+    const [value, setValue] = useState(0); // integer state
+    return () => setValue(value => ++value); // update the state to force render
+}
+
 const generateSubTimerHtml = (timer, formattedDiffInMinPerTimer: string) => {
     const dateStart = new Date(timer.date_start);
 
@@ -160,45 +169,42 @@ const generateSubTimerHtml = (timer, formattedDiffInMinPerTimer: string) => {
     let dateEndString = '';
     if (!timer.isRunning) {
         diffInDays = differenceInDays(new Date(timer.date_end), dateStart);
-        dateEndString = format(new Date(timer.date_end), 'HH:mm:ss');
+        dateEndString = format(new Date(timer.date_end), 'HH:mm');
     }
 
     return (
-        <div className="flex flex-row items-center ml-2">
-            <div className="text-sm">{format(dateStart, 'HH:mm:ss')} - {!timer.isRunning ? diffInDays > 0 ? dateEndString + ' +'+ diffInDays : dateEndString : '...'}</div>
+        <div className="flex flex-row items-center ml-2 mt-1">
+            <div className="text-sm">{format(dateStart, 'HH:mm')}  -  {!timer.isRunning ? diffInDays > 0 ? dateEndString + ' +'+ diffInDays : dateEndString : '...'}</div>
             <div className="text-sm ml-auto text-gray-600">{formattedDiffInMinPerTimer}</div>
         </div>
     )
 }
 
-const generatePagination = (data, curPage) => {
-    let pageIndicatorLow = 0;
-    let pageIndicatorHigh = 0;
-    let maxPages = Math.ceil(data['hydra:totalItems'] / 30);
-    let currentPageNumber = Number(curPage);
-    return (
-        Array.from({length: maxPages}, (_, i) => i + 1).map((page, value) => {
-            console.log(value, page, currentPageNumber);
-            if (page > 1 && page < currentPageNumber -1) {
-                pageIndicatorLow++;
-            }
-            if (page > 1 && page > currentPageNumber +1) {
-                pageIndicatorHigh++;
-            }
+const prepareTimerData = (data, totalSecondsPerDay, timersNormalized) => {
+    {data['hydra:member'].map((value, key) => {
+        typeof value.date_end !== 'undefined' ? value.isRunning = false : value.isRunning = true;
+        const dateStartStr = format(new Date(value.date_start), 'u-MM-dd');
 
-            return (
-                page === 1 || page === maxPages || page === currentPageNumber || page === currentPageNumber-1 || page === currentPageNumber+1 ?
-                    <Link href={`http://localhost:3000/timers?page=${page}`}>
-                        <a className={`w-12 md:flex justify-center items-center hidden cursor-pointer leading-5 transition duration-150 ease-in hover:bg-white rounded-full${cn({' bg-teal-600 text-white hover:text-gray-700': currentPageNumber === page})}`}>{page}</a>
-                    </Link>
-                :
-                    (pageIndicatorLow === 1 && pageIndicatorHigh === 0) || pageIndicatorHigh === 1?
-                    <div className={`w-12 md:flex justify-center items-center hidden cursor-default leading-5 transition duration-150 ease-in rounded-full`}>...</div>
-                :
-                    null
-            )
-        })
-    )
+        let diffInSeconds = 0;
+        if (!value.isRunning) {
+            diffInSeconds = differenceInSeconds(new Date(value.date_end), new Date(value.date_start));
+        } else {
+            diffInSeconds = differenceInSeconds(new Date(), new Date(value.date_start));
+        }
+        value.diffInSeconds = diffInSeconds;
+
+        if (totalSecondsPerDay.hasOwnProperty(dateStartStr)) {
+            totalSecondsPerDay[dateStartStr] += diffInSeconds
+        } else {
+            totalSecondsPerDay[dateStartStr] = diffInSeconds;
+        }
+
+        if (typeof timersNormalized[dateStartStr] === 'undefined') {
+            timersNormalized[dateStartStr] = [];
+        }
+        timersNormalized[dateStartStr].push(value);
+    })}
+
 }
 
 const toHHMMSS = (seconds: number) => {
