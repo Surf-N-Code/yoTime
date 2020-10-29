820fe88c2853395296f0474d320438e9b063b574
Norman <norman@yazio.com>
2020-10-29 18:22:11 +0100

    Add close function when clicking somewhere when menu is open

--- client/components/menu.tsx
+++ client/components/menu.tsx
@@ -11,25 +11,28 @@ type MenuProps = {
 export const Menu = ({ menuIsOpen, onClick }: MenuProps ) => {
     const router = useRouter();
     return (
-        <aside
-            className={`flex flex-col transform fixed top-0 left-64 w-40 h-full bg-white h-full shadow-xl fixed overflow-auto ease-in-out transition-all duration-200 ${cn({'translate-x-0': menuIsOpen}, {'-translate-x-full': !menuIsOpen})}`}
-            onClick={() => onClick()}
-        >
-            <div className="pr-3 bg-white shadow flex items-center h-20">
-                <Link href="/">
-                    <img src="../images/icons/icons8-timer-100.png" width="90" className="pl-2 pr-2 border-r border-teal-100 cursor-pointer" alt="Logo"/>
-                </Link>
-                <img src="../images/icons/close.svg" width="25" className="ml-5 cursor-pointer" alt="Menu" onClick={() => onClick()}/>
-            </div>
-            <ul className="flex flex-col flex-grow mt-4">
-                {generateMainMenuItem('dashboard', true, 'dashboard', 'Dashboard', router, false)}
-                {generateMainMenuItem('timers', false, 'timers', 'Timers', router, false)}
-                {generateMainMenuItem('dailies', false, 'dailies', 'Dailies', router, false)}
-                {generateMainMenuItem('personio', false, 'personio', 'Personio', router, false)}
-                {generateMainMenuItem('settings', false, 'settings', 'Settings', router, true)}
-                {generateMainMenuItem('logout', false, 'logout', 'Logout', router, false)}
-            </ul>
-        </aside>
+        <div className="flex w-full h-full fixed top-0"
+             onClick={() => onClick()}>
+            <aside
+                className={`flex flex-col transform fixed top-0 left-64 w-40 h-full bg-white h-full shadow-xl fixed overflow-auto ease-in-out transition-all duration-200 ${cn({'translate-x-0': menuIsOpen}, {'-translate-x-full': !menuIsOpen})}`}
+                onClick={() => onClick()}
+            >
+                <div className="pr-3 bg-white shadow flex items-center h-20">
+                    <Link href="/">
+                        <img src="../images/icons/icons8-timer-100.png" width="90" className="pl-2 pr-2 border-r border-teal-100 cursor-pointer" alt="Logo"/>
+                    </Link>
+                    <img src="../images/icons/close.svg" width="25" className="ml-5 cursor-pointer" alt="Menu" onClick={() => onClick()}/>
+                </div>
+                <ul className="flex flex-col flex-grow mt-4">
+                    {generateMainMenuItem('dashboard', true, 'dashboard', 'Dashboard', router, false)}
+                    {generateMainMenuItem('timers', false, 'timers', 'Timers', router, false)}
+                    {generateMainMenuItem('dailies', false, 'dailies', 'Dailies', router, false)}
+                    {generateMainMenuItem('personio', false, 'personio', 'Personio', router, false)}
+                    {generateMainMenuItem('settings', false, 'settings', 'Settings', router, true)}
+                    {generateMainMenuItem('logout', false, 'logout', 'Logout', router, false)}
+                </ul>
+            </aside>
+        </div>
     )
 }
 
