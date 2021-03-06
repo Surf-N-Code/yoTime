a19045bc5ed91804d26438706cce1809895f3b87
Norman <norman@yazio.com>
2021-03-06 08:26:30 +0100

    Add reporting endpoint

---
+++ api/tests/Handler/MessageHandler/Slack/ReportingHandlerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Tests\Handler\MessageHandler\Slack;
+
+use App\Entity\User;
+use App\Handler\MessageHandler\Slack\ReportingHandler;
+use App\Services\Time;
+use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Prophecy\Prophecy\Revealer;
+
+class ReportingHandlerTest extends TestCase
+{
+
+    use ProphecyTrait;
+
+    private $time;
+    private $user;
+
+    private ReportingHandler $reportinghandler;
+
+    public function setup(): void
+    {
+        $this->time = $this->prophesize(Time::class);
+        $this->user = $this->prophesize(User::class);
+
+        $this->reportinghandler = new ReportingHandler(
+            $this->time->reveal()
+        );
+    }
+
+    public function testGetStatusByDay()
+    {
+        $this->time->getTimesSpentByTypeAndPeriod($this->user->reveal(), 'day')
+            ->shouldBeCalled()
+            ->willReturn([3600, 600]);
+        $this->reportinghandler->getUserStatus($this->user->reveal(), '/day');
+    }
+}
